41b0411d97563c1291711d445f46cea2895304cc
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-10 14:26:54 +0100

    Go GitHub Actions

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -6,7 +6,7 @@
 #
 # ./demo.sh
 #
-# Assumes that the 'SimplifyNewick' binary is in the current working folder,
+# Assumes that the 'SimplifyNewickDesktop' binary is in the current working folder,
 #
 
 my_newick="((((((((((((10,10),10),10),10),10),10),10),10),10),10),10),10)"
@@ -15,12 +15,12 @@ one_hundred_million=100000000
 number=$million
 
 # Write first result to file
-./SimplifyNewick $my_newick $number > out.txt
+./SimplifyNewickDesktop $my_newick $number > out.txt
 
 number=$((number + million))
 while [ $number -lt $one_hundred_million ]; do
   echo "Number = $number"
   number=$((number + million))
   #Append other results to file
-  ./SimplifyNewick $my_newick $number >> out.txt
+  ./SimplifyNewickDesktop $my_newick $number >> out.txt
 done
